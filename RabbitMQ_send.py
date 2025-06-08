698e5f2570a5568231468fb87aa678482a96e569
liz <angeladeliz@uniplaclages.edu.br>
2025-06-07 23:00:24 -0300

    adicionado explicação do codigo de envio de mensagens para o rabbit

--- RabbitMQ_send.py
+++ RabbitMQ_send.py
@@ -1,21 +1,56 @@
+# Shebang: indica que este script deve ser executado com o interpretador Python
+# Permite executar o arquivo diretamente no terminal: ./script.py
 #!/usr/bin/env python
-import pika
-import random
 
+import pika # Importa a biblioteca Pika para comunicação com RabbitMQ
+import random # Importa biblioteca para gerar números aleatórios
 
-credentials = pika.PlainCredentials('user', 'password')  # 'guest', 'guest'
-parameters = pika.ConnectionParameters('localhost', 5672, '/', credentials)
+# CONFIGURAÇÃO DA CONEXÃO
+# Cria credenciais de autenticação para conectar ao RabbitMQ
+credentials = pika.PlainCredentials('user', 'password')  # 'guest', 'guest' 
+
+# Define os parâmetros de conexão:
+# - 'localhost': endereço do servidor RabbitMQ (servidor local)
+# - 5672: porta padrão do RabbitMQ para conexões AMQP
+# - '/': virtual host padrão (como um namespace) Ex.: /app-vendas, /app-estoque, /prod, dev etc...
+# - credentials: as credenciais criadas acima
+parameters = pika.ConnectionParameters('localhost', 5672, 'dev', credentials)
+
+# Estabelece conexão SÍNCRONA com o RabbitMQ usando os parâmetros
+# BlockingConnection fica "travado" aqui até conseguir conectar
+# Só continua quando a conexão for estabelecida ou der erro
 connection = pika.BlockingConnection(parameters)
+# FIM DA CONFIGURAÇÃO DA CONEXÃO
 
+# CONFIGURAÇÃO DO CANAL
+
+# Cria um canal de comunicação dentro da conexão
+# Canal é onde realmente acontecem as operações (enviar/receber mensagens)
 channel = connection.channel()
-channel.queue_declare(queue='filaTeste')  # declara a fila
 
+# Declara/cria uma fila chamada 'filaTeste'
+# Se a fila já existir, nada acontece
+# Se não existir, ela é criada
+channel.queue_declare(queue='filaTesteDev')  # declara a fila
+#FIM DA CONFIGURAÇÃO DO CANAL
+
+# PREPARAÇÃO DA MENSAGEM
+#Monta o conteudo que será dentro do pacote de mensagem
 strEnviar = 'Mensagem ' + str(random.randrange(0, 100, 1))
+# FIM DA PREPARAÇÃO DA MENSAGEM
 
-# roteia para a fila
+# ENVIO DA MENSAGEM
+# Publica/envia a mensagem para a fila:
+#exchange recebe mensagens de produtores e decide para quais filas elas devem ser entregues
+# - exchange='': usa o exchange padrão (direct exchange)
+# - routing_key='filaTeste': nome da fila de destino
+# - body=strEnviar: conteúdo da mensagem a ser enviada
 channel.basic_publish(exchange='', routing_key='filaTeste', body=strEnviar)
-
+# Se você quiser enviar a mesma mensagem para várias filas ao mesmo tempo, o exchange padrão não serve e
+# precisar criar um exchange customizado, ex: channel.exchange_declare(exchange='log', exchange_type='fanout')
+# fanout = broadcast
 
 print("Enviada: " + strEnviar)
 
+# Fecha a conexão com RabbitMQ
 connection.close()
